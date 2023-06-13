46e303d3fca054b0309661fc152a2d5363cd6a26
Camilo Mejia <camilo.mejia@auto1.com>
2023-06-13 16:35:48 +0200

    Change app

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
-const http = require("http");
-
-const hostname = "127.0.0.1";
+const express = require("express");
+const app = express();
+const hostname = "127.0.0.1"; // Your server ip address
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
+const version = "1.0.0";
+
+app.get("/", (req, res) => {
+  // set response content
+  res.send(`<html>
+                    <body>
+                        <h1 style="color:blue;text-align: center;margin-top: 100px;"> [Version ${version}]: This is AMAZING!!! Like & Subscribe!</h1>
+                        <div style="position: fixed;top: 50%;left: 50%;transform: translate(-50%, -50%)">
+                            <img src="https://picsum.photos/400/400?random=1">
+                        </div>
+                    </body>
+                   </html>`);
+
+  console.log(
+    `[Version ${version}]: New request => http://${hostname}:${port}` + req.url
+  );
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => {
+  console.log(
+    `[Version ${version}]: Server running at http://${hostname}:${port}/`
+  );
 });
